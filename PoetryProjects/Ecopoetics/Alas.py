0d81bf2aedb2969f283aca4e18639996fdd7892f
GLOrozcoM <glorozcom@protonmail.com>
2020-06-02 15:13:15 -0700

    Adding poetry projects

---
+++ PoetryProjects/Ecopoetics/Alas.py
@@ -0,0 +1,349 @@
+def enterB1(event):
+    b.configure(text='Alas the ghost says.')
+
+def leaveB1(event):
+    b.configure(text='I love her, but she does not love me anymore.')
+
+def enterB2(event):
+    b.configure(text='Alas, the Lord of hosts cries.')
+
+def leaveB2(event):
+    b.configure(text='She said I was an asshole, but she did not explain why.')
+
+def enterB3(event):
+    b1.configure(text='Alas, the marble statues pray.')
+
+def leaveB3(event):
+    b1.configure(text='What is left for thee but do well in the sight of they Lord?')
+
+def enterB4(event):
+    b.configure(text='Alas, the merchant of venice sings.')
+
+def leaveB4(event):
+    b.configure(text='Locke responds with his lies.')
+
+def enterB5(event):
+    b.configure(text='Alas, my friend tells me.')
+
+def leaveB5(event):
+    b.configure(text='You are but an abortion of time.')
+
+
+def enterB6(event):
+    b.configure(text='Alas, ring the bells of my neurons.')
+
+
+def leaveB6(event):
+    b.configure(text='Timbre of thought.')
+
+
+def enterB7(event):
+    b.configure(text='Alas, rivers of mercy upon thy head.')
+
+
+def leaveB7(event):
+    b.configure(text='Oh.')
+
+
+def enterB8(event):
+    b.configure(text='Alas, how green was my valley then!')
+
+
+def leaveB8(event):
+    b.configure(text='A mechanism of strong thoughts.')
+
+
+def enterB9(event):
+    b.configure(text='Alas, eloi eloi lama sabactani?')
+
+
+def leaveB9(event):
+    b.configure(text='Though shalt know that I am but human.')
+
+
+def enterB10(event):
+    b.configure(text='Alas, the room of branches doth grow a sprog.')
+
+
+def leaveB10(event):
+    b.configure(text='LOOK NOT AT THE SPECIFIC BUT AT THE PATTERN.')
+
+
+def enterB11(event):
+    b.configure(text='Alas, pray for me.')
+
+
+def leaveB11(event):
+    b.configure(text='May he keep the czar far away from us!')
+
+
+def enterB12(event):
+    b.configure(text='Alas, the tree of the righteouss must be watered.')
+
+
+def leaveB12(event):
+    b.configure(text='The leftouss tree will water itself.')
+
+
+def enterB13(event):
+    b.configure(text='Alas, como un aguila.')
+
+
+def leaveB13(event):
+    b.configure(text='Vuela mi pajaro, vuela.')
+
+
+def enterB14(event):
+    b.configure(text='Alas, the valley descends on my shadow.')
+
+
+def leaveB14(event):
+    b.configure(text='The hyperion cantos.')
+
+
+def enterB15(event):
+    b.configure(text='Alas, the rebels make this river run red.')
+
+
+def leaveB15(event):
+    b.configure(text='How is it that a river runs like a color?')
+
+
+def enterB16(event):
+    b.configure(text='Alas, my fear is of cobwebs and thorns.')
+
+def leaveB16(event):
+    b.configure(text='Death is but a the blind oath of the living.')
+
+
+def enterB17(event):
+    b.configure(text='Alas.')
+
+
+def leaveB17(event):
+    b.configure(text='Who art thou?')
+
+
+def enterB18(event):
+    b.configure(text='Veritas.')
+
+
+def leaveB18(event):
+    b.configure(text='Veritas.')
+
+
+def enterB19(event):
+    b.configure(text='Alas, the smell of popcorn.')
+
+
+def leaveB19(event):
+    b.configure(text='Gregorian chorale which does not console my weary mind.')
+
+
+def enterB20(event):
+    b.configure(text='Alas, friendship lost.')
+
+
+def leaveB20(event):
+    b.configure(text='Independence gained.')
+
+
+def enterB21(event):
+    b.configure(text='Alas, rivendell.')
+
+
+def leaveB21(event):
+    b.configure(text='May it be.')
+
+
+def enterB22(event):
+    b.configure(text='Alas, no te creo.')
+
+
+def leaveB22(event):
+    b.configure(text='Ojala y si.')
+
+
+def enterB23(event):
+    b.configure(text='Alas, dare ga anata wo koroshita?')
+
+
+def leaveB23(event):
+    b.configure(text='Boku dake da zo.')
+
+
+def enterB24(event):
+    b.configure(text='Alas, cry my veins of frustration.')
+
+
+def leaveB24(event):
+    b.configure(text='RULE THYSELF.')
+
+
+def enterB25(event):
+    b.configure(text='RULE THYSELF!')
+
+
+def leaveB25(event):
+    b.configure(text='OFFENSE OVER DEFENSE.')
+
+
+def enterB26(event):
+    b.configure(text='Alas, the road to neverwhere laid down by Roman hands...')
+
+
+def leaveB26(event):
+    b.configure(text='...gone from my memory it has.')
+
+
+def enterB27(event):
+    b.configure(text='Alas, sex stumbles.')
+
+
+def leaveB27(event):
+    b.configure(text='And gender catches the fall.')
+
+
+def enterB28(event):
+    b.configure(text='Alas, I tried.')
+
+
+def leaveB28(event):
+    b.configure(text='And failed.')
+
+
+def enterB29(event):
+    b.configure(text='These are like couplets really: first voice.')
+
+
+def leaveB29(event):
+    b.configure(text='Second voice.')
+
+
+def enterB30(event):
+    b.configure(text='Alas, run to the front.')
+
+
+def leaveB30(event):
+    b.configure(text='Walk to the back.')
+
+
+def enterB31(event):
+    b.configure(text='Alas, another pattern: black.')
+
+
+def leaveB31(event):
+    b.configure(text='WHITE.')
+
+
+def enterB32(event):
+    b.configure(text='Alas, IF ONLY YOU COULD HEAR ME!')
+
+
+def leaveB32(event):
+    b.configure(text='THE PAIN OF MY PATH, THE DREADFUL PURPOSE.')
+
+
+def enterB33(event):
+    b.configure(text='Alas, our purpose is not clear.')
+
+
+def leaveB33(event):
+    b.configure(text='But clarity is overrated.')
+
+
+def enterB34(event):
+    b.configure(text='Alas, my father chanted.')
+
+
+def leaveB34(event):
+    b.configure(text='He grew his beard.')
+
+
+def enterB35(event):
+    b.configure(text='Alas, he rent his vestiment.')
+
+
+def leaveB35(event):
+    b.configure(text='He hated me.')
+
+
+def enterB36(event):
+    b.configure(text='Alas, he did it for love.')
+
+
+def leaveB36(event):
+    b.configure(text='No, I do not know everything.')
+
+
+def enterB37(event):
+    b.configure(text='ALAS.')
+
+
+def leaveB37(event):
+    b.configure(text='S ALA S ALA.')
+
+
+def enterB38(event):
+    b.configure(text='Alas, paganni sold his soul.')
+
+
+def leaveB38(event):
+    b.configure(text='No one wanted mine.')
+
+
+def enterB39(event):
+    b.configure(text='Alas, how could I barter it?')
+
+
+def leaveB39(event):
+    b.configure(text='My pleasure for a bit of oasis in this time desert.')
+
+
+def enterB40(event):
+    b.configure(text='Alas the ghost says.')
+
+
+def leaveB40(event):
+    b.configure(text='Truth, the silence thereof.')
+
+
+def enterB41(event):
+    b.configure(text='Alas, my friend.')
+
+
+def leaveB41(event):
+    b.configure(text='My enemy and his husband, her wife.')
+
+
+def enterB42(event):
+    b.configure(text='Alas, why dost thou not listen?')
+
+
+def leaveB42(event):
+    b.configure(text='Listening is hard Rueven!')
+
+
+def enterB43(event):
+    b.configure(text='Alas, says Danny Saunders.')
+
+
+def leaveB43(event):
+    b.configure(text='Who can hear the world cry.')
+
+
+def enterB44(event):
+    b.configure(text='Alas, my body cries')
+
+
+def leaveB44(event):
+    b.configure(text='ONWARD! My mind demands.')
+
+
+def enterB45(event):
+    b.configure(text='Alas, the ghost says.')
+
+
+def leaveB45(event):
+    b.configure(text='I smile.')
+
---
+++ PoetryProjects/RandomPoetry/Briefcase.py
@@ -0,0 +1,204 @@
+# Program designed to revise 304 poems randomly
+# - I wonder what Atticus carries in the briefcase.
+
+#  Import statements
+# - I still wish there were more new things under this sun
+import os
+import subprocess
+from random import randint
+from random import sample
+from random import choices
+
+# Method definitions
+# -- skyscrapers
+def dig_root():
+    # Acquire the current working directory
+    # - The journey of a thousand cliche's starts with the first one
+    foundation = os.getcwd()
+    # Acquire the files within the current directory
+    # - Humpty's pieces
+    humpty = [f for f in os.listdir(foundation) if os.path.isfile(f)]
+    # Remove the DS store file
+    # - Lollipops and sticky like the sun
+    humpty.pop(0)
+    return humpty
+
+def cast_members(files):
+    # Populate lists to include the files associated with revisions, comments, and originals
+    # - Warehouses of future explosive comments
+    bang_set = list()
+    boom_set = list()
+    crash_set = list()
+    for file in files:
+        # Originals
+        if (".txt" in file and "R" not in file and "C" not in file):
+            bang_set.append(file)
+        # Revisions
+        elif (".txt" in file and "R" == file[0]):
+            boom_set.append(file)
+        # Comments
+        elif (".txt" in file and "C" == file[0]):
+            crash_set.append(file)
+
+    return bang_set, boom_set, crash_set
+
+def charged_field(rev_choice):
+    # Sample how many to revise from the original poetry
+    bang_N = randint(1, len(bang_set))
+    # Sample to pick from in the revisions
+    boom_N = randint(1, len(boom_set))
+    # Sample to pick from in the comments section
+    crash_N = randint(1, len(crash_set))
+
+    # Randomly create a list that will contain the text we want to revise
+    # -- Choice 1
+    tortoise = randint(1, len(bang_set))
+    i = tortoise
+    bang_rev = list()
+    while i < bang_N:
+        bang_rev.append(bang_set[i % bang_N])  # Populate the container for the true originals
+        i += 1  # Today, we avoid the infinite loop
+
+    # Establish the revision choice
+    rev_choice = rev_choice
+
+    # Randomly pick what text you want to use to revise from
+    # Note that we MUST have the exact same number as bang_rev
+    boom_rev = list()
+    hare = len(bang_rev)
+    j = 0
+    while j < hare:
+        # Want to revise from revision text
+        if rev_choice == 1:
+            boom_rev.append(boom_set[j % boom_N])
+        elif rev_choice == 2:
+            boom_rev.append(crash_set[j % crash_N])
+        j += 1
+
+    return bang_rev, boom_rev
+
+def fields(bull, ox_lines, eco_lines):
+    # Revise each file in the bang_rev
+    # -- make certain we are not working empty handed
+    if len(bang_rev) != 0:
+        # - Store all the wonderful lines to revise
+        # --- Note that this will become a 2 dimensional list
+        these_lines = list()
+        for file in bang_rev:
+            # Read the lines in
+            with open(file) as og_file:
+                these_lines.append(og_file.readlines())
+        # - Store all the wonderful lines we will use as revisions
+        those_lines = list()
+        for file in boom_rev:
+            # Read them in!
+            with open(file) as rev_file:
+                those_lines.append(rev_file.readlines())
+
+        # Allow for pulling from oxford and ecopoetics
+        if bull == 2:
+            those_lines = ox_lines
+        elif bull == 3:
+            those_lines = eco_lines
+
+        # We made it thus far.
+        # Now, consider choosing which line to modify
+        # Looping through each file
+        for index in range(0, len(these_lines)):
+
+            # Now in a separate file
+            # -- Want to determine how many lines to revise
+            n_sample = randint(0, len(these_lines[index]))
+            # -- Want to determine which lines to revise
+            og_sample = sample(range(0, len(these_lines[index])), n_sample)
+            rev_sample = choices(range(0, len(those_lines[index])), k=n_sample)
+
+            # Revise the lines we chosen
+            for interior in range(0, n_sample):
+                og_index = og_sample[interior]
+                rev_index = rev_sample[interior]
+                these_lines[index][og_index] = those_lines[index][rev_index]
+
+        # Write out the revised lines in a new file according to its experimented file
+        # Write out the new revised lines
+        for i in range(len(bang_rev)):
+            name = "Investigate" + bang_rev[i]
+            outF = open(name, "w")
+            outF.writelines(these_lines[i])
+            outF.close()
+
+
+
+
+            # Print out sampled sets
+
+# Script in action
+# -- an insect launch
+
+# Get the current directory files and clean theme
+humpty = dig_root()
+
+# Populate the sets that will contain all text
+bang_set, boom_set, crash_set = cast_members(humpty)
+
+# Randomly pick the source for revisions
+rev_choice = randint(1, 2)
+
+# Extract two sets that will revise each other
+bang_rev, boom_rev = charged_field(rev_choice)
+
+# Obtain the Oxford lines
+ox_lines = list()
+with open("Oxford_English_Dictionary.txt") as ox_file:
+    ox_lines.append(ox_file.readlines())
+
+# Obtain the Ecological lines
+eco_lines = list()
+with open("Full_Text.txt") as eco_file:
+    eco_lines.append(eco_file.readlines())
+
+# Perform, now.
+my_bull = randint(1, 3)
+fields(my_bull, ox_lines, eco_lines)
+
+
+print(bang_rev)
+print(boom_rev)
+
+
+### Walking with dust mites
+#with open(bang_set[0]) as f:
+ #   og_lines = f.readlines()
+
+#with open(bang_set[1]) as f2:
+ #   og_rev_lines = f2.readlines()
+
+# Switch lines
+#og_lines[0] = og_rev_lines[3]
+
+
+### Understand that this is now in the past...
+# Write out the new revised lines
+#outF = open("experiment.txt", "w")
+#outF.writelines(og_lines)
+#outF.close()
+
+### Make note to revise the following or not...
+#print(lines)
+#print(len(lines))
+
+
+### Ruins of a previous metropolis
+#print(bang_set)
+#print(boom_set)
+#print(crash_set)
+
+
+### Make note to revise the following or just leave it there...
+## Decide not to include
+# Open the desired file
+# - On a new adventure we go!
+#subprocess.call(['open', '-a', 'TextEdit', files[3]])
+#subprocess.call(['open', '-a', 'TextEdit', files[7]])
+
+# Simultaneously open these?
