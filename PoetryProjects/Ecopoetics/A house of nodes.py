3ee16b3e404b9ab48a7688a768609df166523d01
GLOrozcoM <glorozcom@protonmail.com>
2020-06-03 20:15:43 -0700

    Organized folders. Reflected in code.

--- PoetryProjects/Ecopoetics/A house of nodes.py
+++ PoetryProjects/Ecopoetics/A house of nodes.py
@@ -7,9 +7,6 @@
 # --There is nothing new under the moon.
 from tkinter import *
 from tkinter.ttk import *
-import os
-from random import *
-from time import *
 from Somnium import *
 
 
@@ -102,40 +99,40 @@ def fukai():
     """fukai is a procedural function so no parameters or returns statements are found in it. """
 
     # The original texts.
-    read_image("01.gif")
-    read_image("02.gif")
-    read_image("03.gif")
-    read_image("04.gif")
-    read_image("05.gif")
-    read_image("06.gif")
-    read_image("07.gif")
-    read_image("08.gif")
-    read_image("09.gif")
-    read_image("10.gif")
-    read_image("11.gif")
-    read_image("12.gif")
-    read_image("13.gif")
-    read_image("14.gif")
-    read_image("15.gif")
-    read_image("16.gif")
-    read_image("17.gif")
-    read_image("18.gif")
-    read_image("19.gif")
-    read_image("20.gif")
-    read_image("21.gif")
-    read_image("22.gif")
-    read_image("23.gif")
-    read_image("24.gif")
-    read_image("25.gif")
-    read_image("26.gif")
-    read_image("27.gif")
-    read_image("28.gif")
-    read_image("29.gif")
-    read_image("30.gif")
-    read_image("31.gif")
-    read_image("32.gif")
-    read_image("33.gif")
-    read_image("34.gif")
+    read_image("OriginalEntries/01.gif")
+    read_image("OriginalEntries/02.gif")
+    read_image("OriginalEntries/03.gif")
+    read_image("OriginalEntries/04.gif")
+    read_image("OriginalEntries/05.gif")
+    read_image("OriginalEntries/06.gif")
+    read_image("OriginalEntries/07.gif")
+    read_image("OriginalEntries/08.gif")
+    read_image("OriginalEntries/09.gif")
+    read_image("OriginalEntries/10.gif")
+    read_image("OriginalEntries/11.gif")
+    read_image("OriginalEntries/12.gif")
+    read_image("OriginalEntries/13.gif")
+    read_image("OriginalEntries/14.gif")
+    read_image("OriginalEntries/15.gif")
+    read_image("OriginalEntries/16.gif")
+    read_image("OriginalEntries/17.gif")
+    read_image("OriginalEntries/18.gif")
+    read_image("OriginalEntries/19.gif")
+    read_image("OriginalEntries/20.gif")
+    read_image("OriginalEntries/21.gif")
+    read_image("OriginalEntries/22.gif")
+    read_image("OriginalEntries/23.gif")
+    read_image("OriginalEntries/24.gif")
+    read_image("OriginalEntries/25.gif")
+    read_image("OriginalEntries/26.gif")
+    read_image("OriginalEntries/27.gif")
+    read_image("OriginalEntries/28.gif")
+    read_image("OriginalEntries/29.gif")
+    read_image("OriginalEntries/30.gif")
+    read_image("OriginalEntries/31.gif")
+    read_image("OriginalEntries/32.gif")
+    read_image("OriginalEntries/33.gif")
+    read_image("OriginalEntries/34.gif")
 
 
 # Room of words.
@@ -232,7 +229,7 @@ def learn():
     b45 = Button(master, command=false_path_entry)
     b45.grid(row=0, column=3, sticky='W', pady=4)
 
-# AI's that will write in background. joanna_bieri@redlands.edu
+# AI's that will write in background.
 # -- How do you know that you didn't know?
 def daemon_full_tower():
     """Upon this house do I spiral my continuous tower of language."""
@@ -286,103 +283,103 @@ def path_entry():
         learn()
 
     if e1.get() == "I":
-        read_poem_replace("Sonder's_eyes.txt")
+        read_poem_replace("TypedEntries/Sonder's_eyes.txt")
 
     if e1.get() == "will":
-        read_poem_replace("Maybe_Cycles.txt")
+        read_poem_replace("TypedEntries/Maybe_Cycles.txt")
 
     if e1.get() == "love":
-        read_poem_replace("Prometheus.txt")
+        read_poem_replace("TypedEntries/Prometheus.txt")
 
     if e1.get() == "you":
-        read_poem_replace("Octopi_rain.txt")
+        read_poem_replace("TypedEntries/Octopi_rain.txt")
 
     if e1.get() == "if":
-        read_poem_replace("Ifs.txt")
+        read_poem_replace("TypedEntries/Ifs.txt")
 
     if e1.get() == "wEvBm7":
-        read_poem_replace("The_4_0.txt")
+        read_poem_replace("TypedEntries/The_4_0.txt")
 
     if e1.get() == "never":
-        read_poem_replace("The_bind.txt")
+        read_poem_replace("TypedEntries/The_bind.txt")
 
     if e1.get() == "see":
-        read_poem_replace("Cricket_leg.txt")
+        read_poem_replace("TypedEntries/Cricket_leg.txt")
 
     if e1.get() == "e8DwMR":
-        read_poem_replace("Blithe_trash.txt")
+        read_poem_replace("TypedEntries/Blithe_trash.txt")
 
     if e1.get() == "Dt2xzm":
-        read_poem_replace("Child_bird.txt")
+        read_poem_replace("TypedEntries/Child_bird.txt")
 
     if e1.get() == ",":
-        read_poem_replace("Move_appropriately.txt")
+        read_poem_replace("TypedEntries/Move_appropriately.txt")
 
     if e1.get() == "and":
-        read_poem_replace("No_digas.txt")
+        read_poem_replace("TypedEntries/No_digas.txt")
 
     if e1.get() == "VSFGAv":
-        read_poem_replace("Rain_remember.txt")
+        read_poem_replace("TypedEntries/Rain_remember.txt")
 
     if e1.get() == "x6UbBB":
-        read_poem_replace("Theatre.txt")
+        read_poem_replace("TypedEntries/Theatre.txt")
 
     if e1.get() == "ShGBuV":
-        read_poem_replace("Cabeza.txt")
+        read_poem_replace("TypedEntries/Cabeza.txt")
 
     if e1.get() == "4MsVfG":
-        read_poem_replace("Jungle.txt")
+        read_poem_replace("TypedEntries/Jungle.txt")
 
     if e1.get() == "L6UjcF":
-        read_poem_replace("Liquor_apple.txt")
+        read_poem_replace("TypedEntries/Liquor_apple.txt")
 
     if e1.get() == "z9Rqux":
-        read_poem_replace("Words_house.txt")
+        read_poem_replace("TypedEntries/Words_house.txt")
 
     if e1.get() == "rGpVBU":
-        read_poem_replace("Aztec.txt")
+        read_poem_replace("TypedEntries/Aztec.txt")
 
     if e1.get() == "g6zCvu":
-        read_poem_replace("Endgame.txt")
+        read_poem_replace("TypedEntries/Endgame.txt")
 
     if e1.get() == "every":
-        read_poem_replace("Outerness.txt")
+        read_poem_replace("TypedEntries/Outerness.txt")
 
     if e1.get() == "Tuesday":
-        read_poem_replace("Raining.txt")
+        read_poem_replace("TypedEntries/Raining.txt")
 
     if e1.get() == "reading":
-        read_poem_replace("Ifs.txt")
+        read_poem_replace("TypedEntries/Ifs.txt")
 
     if e1.get() == "this":
-        read_poem_replace("Maps.txt")
+        read_poem_replace("TypedEntries/Maps.txt")
 
     if e1.get() == "not":
-        read_poem_replace("My_mind_similar.txt")
+        read_poem_replace("TypedEntries/My_mind_similar.txt")
 
     if e1.get() == "wearing":
-        read_poem_replace("Couple_smoked.txt")
+        read_poem_replace("TypedEntries/Couple_smoked.txt")
 
     if e1.get() == "a":
-        read_poem_replace("Listen_to_the_clouds.txt")
+        read_poem_replace("TypedEntries/Listen_to_the_clouds.txt")
 
     if e1.get() == "blindfold":
-        read_poem_replace("The_function_playground.txt")
+        read_poem_replace("TypedEntries/The_function_playground.txt")
 
     if e1.get() == "mientras":
-        read_poem_replace("The_poop_proof.txt")
+        read_poem_replace("TypedEntries/The_poop_proof.txt")
 
     if e1.get() == "hay":
-        read_poem_replace("Sir.txt")
+        read_poem_replace("TypedEntries/Sir.txt")
 
     if e1.get() == "esperanza":
-        read_poem_replace("Set_ignorance.txt")
+        read_poem_replace("TypedEntries/Set_ignorance.txt")
 
     if e1.get() == "UK38mz":
-        read_poem_replace("Bloody_pen.txt")
+        read_poem_replace("TypedEntries/Bloody_pen.txt")
 
     if e1.get() == "vida":
-        read_poem_replace("The_function_playground.txt")
+        read_poem_replace("TypedEntries/The_function_playground.txt")
 
 # False path taken if the user presses the wrong button.
 # -- There is only data and flow of information in a poem.
@@ -411,7 +408,7 @@ def false_path_entry():
 
     # Display the new poem to the window.
     # -- Not too late to learn something new.
-    p1 = open("Effendi.txt", 'r')
+    p1 = open("TypedEntries/Effendi.txt", 'r')
     p1_lines = p1.readlines()
     p1_string = ''.join(p1_lines)
     Label(new_master, text=p1_string).grid(row=0)
