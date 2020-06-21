143cb2e04227274a5cb1baa2ed5ca4515edb082d
GLOrozcoM <glorozcom@protonmail.com>
2020-06-21 10:58:50 -0700

    Refactored

--- CoverChessBoard/DevelopBoard.py
+++ CoverChessBoard/DevelopBoard.py
@@ -25,7 +25,6 @@ class GridBoard(App):
         j = int(ij[1])
         return i,j
 
-
     def place_piece_at_index(self, square, piece, index, children):
         """ Children list index
 
@@ -48,40 +47,44 @@ class GridBoard(App):
         return color
 
     def square_pressed(self, instance):
-        print("Pressed!")
         board = instance.parent
-        index_of_curr_square = board.children.index(instance)
-        for child in board.children:
-            if isinstance(child, Piece):
-                piece = child
-                index_occupied_square = board.children.index(piece)
-                board.remove_widget(piece)
+        current_square_index = board.children.index(instance)
 
-                color = self.color_square(piece.square.position)
-                occupied_square = Square(piece.square.position, color)
-                occupied_square.bind(on_release = self.square_pressed)
-                board.add_widget(occupied_square, index_occupied_square)
+        # TODO find faster way to select a piece from the board
+        # --> a way to pass the board object and then immediately select the piece.
+        # --> find out how to use lambda and partial
+        for square in board.children:
+            if isinstance(square, Piece):
+                piece = square
+                self.replace_piece_square(board, square)
 
-                # Remove the old square, place new piece there
-        self.place_piece_at_index(instance, piece, index_of_curr_square, board)
+        # Remove the old square, place new piece there
+        self.place_piece_at_index(instance, piece, current_square_index, board)
 
-        # Redo bindings
+    def replace_piece_square(self, board, piece):
+        occupied_square_index = board.children.index(piece)
+        board.remove_widget(piece)
 
-        return None
+        color = self.color_square(piece.square.position)
+        occupied_square = Square(piece.square.position, color)
+        occupied_square.bind(on_release=self.square_pressed)
+
+        board.add_widget(occupied_square, occupied_square_index)
 
     def bind_squares_in_board(self, board):
-        for child in board.squares.children:
-            if not isinstance(child, Piece):
-                child.bind(on_release = self.square_pressed)
+        for square in board.squares.children:
+            if not isinstance(square, Piece):
+                square.bind(on_release = self.square_pressed)
 
     def build(self):
+        # Create the board
         white = [1, 1, 1, 1]
         mild_green = [0, 0.6, 0.29, 1]
         board = ChessBoard(white, mild_green)
 
+        # Give the board a piece
         piece = Piece("Piece", [1, 0, 0, 1])
         board = board.place_piece(1, 1, piece)
-
 
         self.bind_squares_in_board(board)
 
