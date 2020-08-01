5417fa2fc83b495e870551a882435029debb8f07
GLOrozcoM <glorozcom@protonmail.com>
2020-07-31 21:29:31 -0700

    Add reset board button

--- CoverChessBoard/src/GUI/ComputerBoardScreen.py
+++ CoverChessBoard/src/GUI/ComputerBoardScreen.py
@@ -23,7 +23,7 @@ class BoardScreen(Screen):
         super().__init__(name=name)
 
         # A chess board object, not just the squares
-        self. board = None
+        self.board = None
 
 
 def setup_board():
@@ -46,6 +46,7 @@ def setup_computer_board_screen(screen_manager):
     :return: The screen containing the board where a computer can show the solution.
     """
     layout = BoxLayout(orientation='vertical')
+
     # Give background image to layout
     with layout.canvas.before:
         Color(0, 0, 0.14, 1)
@@ -59,10 +60,15 @@ def setup_computer_board_screen(screen_manager):
 
     # Buttons
     btn_layout = BoxLayout(orientation='horizontal')
-    start_btn = create_solution_button()
+
+    start_solution_btn = create_solution_button()
     back_btn = create_back_button(screen_manager)
+    reset_btn = create_reset_button(screen_manager)
+
     btn_layout.add_widget(back_btn)
-    btn_layout.add_widget(start_btn)
+    btn_layout.add_widget(start_solution_btn)
+    btn_layout.add_widget(reset_btn)
+
     btn_layout.size_hint = (1, 0.2)
 
     layout.add_widget(btn_layout)
@@ -88,7 +94,7 @@ def start_solution_callback(instance, *largs):
             start_position = obj.square.position
     # Only if a piece is present is a solution found for it
     if board.piece:
-        move_list = get_path(start_position, "../solution_paths/knight_tour.txt")
+        move_list = get_path(start_position, "../../solution_paths/knight_tour.txt")
         seconds = 0.5
         for move in move_list:
             Clock.schedule_once(partial(board.move_piece_to_str_ij, move, board.piece), seconds)
@@ -110,3 +116,36 @@ def create_solution_button():
                        color=(1, 1, 1, 0.7))
     start_btn.bind(on_release=start_solution_callback)
     return start_btn
+
+
+# TODO callback
+def reset_button_callback(instance, screen_manager):
+    """ Activate the board resetting process.
+
+    :return: None
+    """
+    # Get the board associated with this screen
+    # -- go from current button, to grid layout, to screen
+    layout_for_screen = instance.parent.parent
+
+    # Remove previous board items
+    layout_for_screen.children.pop()
+    layout_for_screen.children.pop()
+
+    new_board_screen = setup_computer_board_screen(screen_manager)
+    layout_for_screen.add_widget(new_board_screen)
+    return
+
+def create_reset_button(screen_manager):
+    """ Make a button that will reset the board for the user to try a different solution.
+
+    :return: A button to reset the board.
+    """
+    reset_btn = Button(text="Reset board",
+                       pos=(100, 100),
+                       background_normal='',
+                       background_down='',
+                       background_color=(0, 0, 0.14, 1),
+                       color=(1, 1, 1, 0.7))
+    reset_btn.bind(on_release=partial(reset_button_callback, screen_manager=screen_manager))
+    return reset_btn
