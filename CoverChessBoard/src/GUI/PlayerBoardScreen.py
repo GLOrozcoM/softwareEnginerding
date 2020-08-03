2b6d0b851370b3f64f0969751b2652b1475f0c24
GLOrozcoM <glorozcom@protonmail.com>
2020-08-02 17:32:59 -0700

    Give a reset button for board

--- CoverChessBoard/src/GUI/PlayerBoardScreen.py
+++ CoverChessBoard/src/GUI/PlayerBoardScreen.py
@@ -57,8 +57,10 @@ def create_player_board_layout(screen_manager):
     btn_layout = BoxLayout(orientation='horizontal')
     undo_btn = create_undo_button(board)
     back_btn = create_back_button(screen_manager)
+    reset_btn = create_reset_button(screen_manager)
     btn_layout.add_widget(back_btn)
     btn_layout.add_widget(undo_btn)
+    btn_layout.add_widget(reset_btn)
     btn_layout.size_hint = (1, 0.2)
 
     layout.add_widget(board.squares)
@@ -78,3 +80,35 @@ def setup_player_screen(screen_manager):
     player_screen.add_widget(player_board_layout)
     return player_screen
 
+
+def reset_button_callback(instance, screen_manager):
+    """ Activate the board resetting process.
+
+    :return: None
+    """
+    # Get the board associated with this screen
+    # -- go from current button, to grid layout, to screen
+    layout_for_screen = instance.parent.parent
+
+    # Remove previous board layout　and buttons
+    layout_for_screen.children.pop()
+    layout_for_screen.children.pop()
+
+    new_board_screen = setup_player_screen(screen_manager)
+    layout_for_screen.add_widget(new_board_screen)
+    return
+
+
+def create_reset_button(screen_manager):
+    """ Make a button that will reset the board for the user to try a different solution.
+
+    :return: A button to reset the board.
+    """
+    reset_btn = Button(text="Reset board",
+                       pos=(100, 100),
+                       background_normal='',
+                       background_down='',
+                       background_color=(0, 0, 0.14, 1),
+                       color=(1, 1, 1, 0.7))
+    reset_btn.bind(on_release=partial(reset_button_callback, screen_manager=screen_manager))
+    return reset_btn
